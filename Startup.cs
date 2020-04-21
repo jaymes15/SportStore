08c694b8d4cde4a3c5ade4a3fb03653d383f2cc2
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-04-20 23:02:37 -0700

    Disable endpoint routing & creating viewimport

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,7 @@ namespace SportStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
