aa75c6801acb05d3c7879722474e2c74c98b33b4
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-04-20 23:40:29 -0700

    Configure productcontroller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+    }
+}
