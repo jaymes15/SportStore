a5ab428a9f3a3549e60d50ba3576231a6950b96e
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-04-22 11:09:32 -0700

    Database Configuration

--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ namespace SportStore
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseDefaultServiceProvider(options =>options.ValidateScopes = false);
                 });
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Http;
 using SportStore.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 
 namespace SportStore
 {
@@ -15,9 +18,16 @@ namespace SportStore
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddDbContext<ApplicationDbContext>(options => options
+                 .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddTransient<IProductRepository, FakeProductRepository>();
 
         }
